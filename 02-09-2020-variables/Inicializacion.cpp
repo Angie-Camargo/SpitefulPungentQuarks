489ccc6afcb958d67f1db6fb9531454d0f90b478
Angie Camargo <adcamargou@unal.edu.co>
2020-09-04 20:26:14 +0000

    inicializacion y declaracion de variables

---
+++ 02-09-2020-variables/Inicializacion.cpp
@@ -0,0 +1,9 @@
+//programa de ejemplo
+#include <iostream>
+/* en este caso utilizo int main(int argc, char **argv)  porque esto permite que cuando ejecuto un programa le pongo argumentos en la linea de comando como números */ 
+int main(int argc, char **argv) 
+//argc nos dice cuantos argumentos usamos y char cadenas de texto
+{
+int a =20; // declaración e inicialización de una variable de tipo entero
+  return 0;
+}
